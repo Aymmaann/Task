d4e6ab1ab08898ee735bc24b3ef217d603fc4633
Ayman Haseeb <aymanhaseeb8121@gmail.com>
2025-03-27 23:40:25 +0530

    minor style updation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
                 )}
 
                 {displayOptions && (
-                    <div className='absolute top-10 right-0 border border-1 border-zinc-200 bg-darkBlue rounded-md p-4 min-w-[180px] z-10'>
+                    <div className='absolute top-10 right-0 border border-1 border-zinc-800 bg-darkBlue rounded-md p-4 min-w-[180px] z-10'>
                         <p className='text-sm'>{JSON.parse(localStorage.getItem('user')).name || JSON.parse(localStorage.getItem('user')).email}</p>
                         <button className='text-center text-sm w-full bg-teal-900 py-1.5 rounded-md mt-5 hover:bg-teal-800 smoothTransition' onClick={handleLogout}>
                             <span className=''>Log out</span>
