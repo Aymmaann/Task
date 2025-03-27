6f0f2a2774fc42c8144ab68924d8d46ab71ba4a1
Ayman Haseeb <aymanhaseeb8121@gmail.com>
2025-03-27 23:48:54 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Login from './pages/Login'
 import { Navigate, Route, Routes, useNavigate } from 'react-router-dom'
 import Details from './pages/Details'
 import Signup from './pages/Signup'
-import { UserProvider } from './context/userContext'
+import { UserProvider } from './context/UserContext'
 
 const ProtectedRoute = ({children}) => {
   const user = JSON.parse(localStorage.getItem('user'))
